eca583dbc9c26a1ef738e0753ce0382dc0848584
shahdhamad <shhdh5296@gmail.com>
2024-05-21 11:38:36 +0300

    add

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
-import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
