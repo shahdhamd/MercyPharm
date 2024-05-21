e8a565bc5bbca8222402fc29073aa0c91f484dc4
shahdhamad <shhdh5296@gmail.com>
2024-05-21 11:36:07 +0300

    addFontawesome

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
